ee86c0fb878b0d6a6ed29c1413e0ff861fb2d8ec
Pierre Olivier Tardif <pierre.o.tardif+git@gmail.com>
2018-05-25 20:06:30 -0400

    Replace the association list with an array and a hash table.

--- input.ml
+++ input.ml
@@ -1,34 +1,33 @@
 open Util
 
 (** Bindings between the CHIP-8 keys and the actual keyboard keys. *)
-let bindings = [
-  (0x0, Sdlkey.KEY_KP0);
-  (0x1, Sdlkey.KEY_KP7);
-  (0x2, Sdlkey.KEY_KP8);
-  (0x3, Sdlkey.KEY_KP9);
-  (0x4, Sdlkey.KEY_KP4);
-  (0x5, Sdlkey.KEY_KP5);
-  (0x6, Sdlkey.KEY_KP6);
-  (0x7, Sdlkey.KEY_KP1);
-  (0x8, Sdlkey.KEY_KP2);
-  (0x9, Sdlkey.KEY_KP3);
-  (0xA, Sdlkey.KEY_KP_DIVIDE);
-  (0xB, Sdlkey.KEY_KP_MULTIPLY);
-  (0xC, Sdlkey.KEY_KP_MINUS);
-  (0xD, Sdlkey.KEY_KP_PLUS);
-  (0xE, Sdlkey.KEY_KP_ENTER);
-  (0xF, Sdlkey.KEY_KP_PERIOD);
-]
+let bindings = [|
+  (*0x0*) Sdlkey.KEY_KP0;
+  (*0x1*) Sdlkey.KEY_KP7;
+  (*0x2*) Sdlkey.KEY_KP8;
+  (*0x3*) Sdlkey.KEY_KP9;
+  (*0x4*) Sdlkey.KEY_KP4;
+  (*0x5*) Sdlkey.KEY_KP5;
+  (*0x6*) Sdlkey.KEY_KP6;
+  (*0x7*) Sdlkey.KEY_KP1;
+  (*0x8*) Sdlkey.KEY_KP2;
+  (*0x9*) Sdlkey.KEY_KP3;
+  (*0xA*) Sdlkey.KEY_KP_DIVIDE;
+  (*0xB*) Sdlkey.KEY_KP_MULTIPLY;
+  (*0xC*) Sdlkey.KEY_KP_MINUS;
+  (*0xD*) Sdlkey.KEY_KP_PLUS;
+  (*0xE*) Sdlkey.KEY_KP_ENTER;
+  (*0xF*) Sdlkey.KEY_KP_PERIOD;
+|]
 
 (** Find the keyboard key associated to a given CHIP-8 key. *)
-let keysym_of_int n =
-  assert (in_range n (0x0, 0xF));
-  List.assoc n bindings
+let keysym_of_int = Array.get bindings
 
 (** Find the CHIP-8 key associated to a given keyboard key. *)
-let int_of_keysym keysym =
-  let (n, _) = List.find (fun (_, k) -> k = keysym) bindings in
-  n
+let int_of_keysym =
+  let ht = Hashtbl.create 16 in
+  Array.iteri (fun i x -> Hashtbl.add ht x i) bindings;
+  Hashtbl.find ht
 
 (** Process the events in the event queue until there are no more. *)
 let rec process_events ?(paused=false) () =
